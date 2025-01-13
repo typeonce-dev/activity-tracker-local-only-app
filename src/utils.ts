c1af880f455bbad4993084a1dc802b16c99360ca
SandroMaglione <lass.maglio@gmail.com>
2025-01-13 18:36:31 +0100

    `Schema.compose`

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
-import { Either, ParseResult, Schema, type Array } from "effect";
+import { Schema, type Array } from "effect";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -10,17 +10,4 @@ export const LiteralFromString = <
   Literals extends Array.NonEmptyReadonlyArray<string>,
 >(
   literal: Schema.Literal<Literals>
-) =>
-  Schema.String.pipe(
-    Schema.transformOrFail(literal, {
-      decode: (from, _, ast) =>
-        Schema.decodeUnknownEither(literal)(from).pipe(
-          Either.match({
-            onLeft: (error) =>
-              ParseResult.fail(new ParseResult.Type(ast, from, error.message)),
-            onRight: ParseResult.succeed,
-          })
-        ),
-      encode: ParseResult.succeed,
-    })
-  );
+) => Schema.compose(Schema.String, literal);
